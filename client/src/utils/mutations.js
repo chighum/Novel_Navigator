9c3f4d4a05e036849631f5e14143b619c037fb18
Charlie Highum <chighum41@gmail.com>
2022-01-30 20:24:38 -0700

    finished mutations.js on client side with functions to SAVE_BOOK and REMOVE_BOOK

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,33 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $authors: [String]!
+    $description: String!
+    $title: String!
+    $bookId: String!
+    $image: String!
+    $link: String!
+  ) {
+    saveBook(
+      authors: $authors
+      description: $description
+      title: $title
+      bookId: $bookId
+      image: $image
+      link: $link
+    ) {
+      user
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      user
+    }
+  }
+`;
